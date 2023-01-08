1e26ecc03c4f3333dded59b61b0f8433ef1e9062
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2023-01-08 19:46:02 +0100

    add_environnement_init_script

---
+++ init.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# This script is used to setup the environment for the C-ROULETTE
+
+player_username="player"
+player_password="roulette"
+
+echo "--------------------------------------------------------------------------------"
+echo "Setting up the environment for C-ROULETTE"
+echo "--------------------------------------------------------------------------------"
+
+echo "Creating the player user"
+egrep "^player" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then   
+    echo "User player already exist"
+else
+    password=$(perl -e 'print crypt($ARGV[0], "password")' $player_password)
+    useradd -s /bin/bash -m -p "$password" "$player_username"
+    if [ $? -ne 0 ]; then
+        echo "Player account created"
+    fi
+    echo "Adding alias PRPLACE"
+    echo "alias PRPLACE='cd /home/ubuntu/C-Roulette;./player.s'" >> /home/player/.bashrc
+fi
+echo "--------------------------------------------------------------------------------"
+
+echo "Setup script done"
+
+./dealer.sh
+
+echo "You can now use the command 'PROULETTE yourname' to start playing !"

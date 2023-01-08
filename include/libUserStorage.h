8cc9d28c03d09dd18712f78e75a2cb7fa9bc12fe
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2023-01-08 19:15:56 +0100

    feat: add user already connected check

--- include/libUserStorage.h
+++ include/libUserStorage.h
@@ -9,3 +9,7 @@ int userAlreadyExist();
 void addUserInformation(char *name, int money);
 void updateUserInformation(char *name, int money);
 int userOnboarding(char *name, int money);
+int getUserMoney(char *name);
+int userIsAlreadyConnected(char *name);
+void connectUser(char *name);
+void disconnectUser(char *name);

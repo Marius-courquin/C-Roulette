5a74a1c44ca40a3233ba16a48f58669622ad8cf7
charles <charles25.bournonville@gmail.com>
2023-01-08 21:08:45 +0100

    fix: remove bug when user attempts to connect with the same username

--- include/libUserStorage.h
+++ include/libUserStorage.h
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #define USER_STORAGE_PATH "userStorage"
+#define STARTING_MONEY 1000
 
 FILE *openFile(char *fileName, char *mode);
 int readUserInformation(char *name);
 int userAlreadyExist();
 void addUserInformation(char *name, int money);
 void updateUserInformation(char *name, int money);
-int userOnboarding(char *name, int money);
+int userOnboarding(char *name, int *money);
 int getUserMoney(char *name);
 int userIsAlreadyConnected(char *name);
 void connectUser(char *name);
